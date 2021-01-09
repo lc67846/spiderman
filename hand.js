7360a89b99b60d54ec21ee201d928c7bbea86cb1
#Wifi Got Minecraft <64691261+lc67846@users.noreply.github.com>
2021-01-09 04:08:14 -0600

    Fixed The Hands

--- hand.js
+++ hand.js
@@ -67,23 +67,7 @@ AFRAME.registerComponent("hand", {
       self.handleButton("trackpad", "down", evt.detail);
     };
     this.onTrackpadUp = function(evt) {
-      self.handleButton("trackpad", "up", AFRAME.registerComponent('random-color', {
-    dependencies: ['material'],
-
-    Ifbuttonclick then return colors;
-    init: function(){
-        this.el.setAttribute('material', 'color', getRandomColor());
-    }
-});
-
-function getRandomColor(){
-    var letters = '0123456789ABCDEF'
-    var colors = "#"
-    for(var i = 0; i < 6; i++){
-        colors += letters[Math.floor(Math.random() * 16)]
-    }
-    return colors;
-});
+      self.handleButton("trackpad", "up", evt.detail);
     };
     this.onTrackpadChanged = function(evt) {
       self.handleButton("trackpad", "changed", evt.detail);
